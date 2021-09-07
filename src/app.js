1da55c798664d2c4896ddf77ee95e5e0b5162a9e
Harvinder Singh <rajputharvi@gmail.com>
2021-09-07 11:05:19 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -33,6 +34,6 @@ app.get('/weather',(req,res) => {
     res.send("Your Weather");
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log('Server is running on port'+ port);
 })
